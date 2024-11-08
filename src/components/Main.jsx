c89abda893eb62c4d735637a67c82606f6f5eed0
AkashKumar-0 <ahmvaad@gmail.com>
2024-11-08 23:40:17 +0530

    optimised reload fetching

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,75 +1,82 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useAuth } from '@clerk/clerk-react'
-import { useStore } from '../store'
-import TodoList from './TodoList'
-import URLInput from './URLInput'
-import SignOut from './SignOut'
-import Spinner from './shared/Spinner'
-import { useEffect, useState } from 'react'
-import ToggleSearch from './ToggleSearch'
-import { motion } from 'framer-motion'
-import {useAxiosInstance} from "../api/axios.js"
-import {endpoints} from "../api/config.js"
+import { useAuth } from "@clerk/clerk-react";
+import { useStore } from "../store";
+import TodoList from "./TodoList";
+import URLInput from "./URLInput";
+import SignOut from "./SignOut";
+import Spinner from "./shared/Spinner";
+import { useEffect, useState } from "react";
+import ToggleSearch from "./ToggleSearch";
+import { motion } from "framer-motion";
+import { useAxiosInstance } from "../api/axios.js";
+import { endpoints } from "../api/config.js";
 
-import debounce from 'debounce'
+import debounce from "debounce";
 
 export default function Main() {
-  const [toggleSearch, setToggleSearch] = useState(true)
-  const [error , setError] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const { userId }= useAuth()
+  const [toggleSearch, setToggleSearch] = useState(true);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { userId } = useAuth();
 
-  const api = useAxiosInstance()
+  const api = useAxiosInstance();
   //console.log(userId, token)
 
   // const currentUser = user.externalAccounts[0]
   // console.log('userId', user:width: ,Id, isSignedIn)
 
-  const {tasks, setTasks,  setFilteredTask ,createNewTaskFlag, updateTaskFlag } = useStore((state) => {
+  const {
+    tasks,
+    setTasks,
+    setFilteredTask,
+    createNewTaskFlag,
+    updateTaskFlag,
+  } = useStore((state) => {
     return {
       tasks: state.tasks,
       setTasks: state.setTasks,
       createNewTaskFlag: state.createNewTaskFlag,
       updateTaskFlag: state.updateTaskFlag,
-      setFilteredTask : state.setFilteredTask,
-    }
-  })
+      setFilteredTask: state.setFilteredTask,
+    };
+  });
   // call the backend api to store the user id as soon as the user logs in
   const getAllItems = async () => {
     try {
-     setLoading(true)
-      const res = await api.get(endpoints.getAll(userId))
-      const { userData } = res.data
+      if (tasks.length === 0) setLoading(true);
+      else setFilteredTask(tasks);
+      const res = await api.get(endpoints.getAll(userId));
+      const { userData } = res.data;
       // console.log('data', userData)
-      setTasks(userData)
-      setFilteredTask(userData)
+      setTasks(userData);
+      setFilteredTask(userData);
       // console.log('fetched data from ../getAll', userData)
     } catch (error) {
-      console.error('error', error)
-      setError(true)
+      console.error("error", error);
+      setError(true);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
   const handleCreateNewNote = () => {
     //console.log('Craete new note')
-    updateTaskFlag(!createNewTaskFlag)
-  }
+    updateTaskFlag(!createNewTaskFlag);
+  };
 
   const handleSearch = debounce((query) => {
-  //console.log(query)
+    //console.log(query)
     const filteredTask = tasks.filter(
-      (item) => item.title.includes(query) || item.url.includes(query)
-    )
-    setFilteredTask(filteredTask)
+      (item) => item.title.includes(query) || item.url.includes(query),
+    );
+    setFilteredTask(filteredTask);
     //console.log(filteredTask)
-  }, 300) 
+  }, 300);
 
   useEffect(() => {
-    getAllItems()
-    
-    localStorage.setItem('userId', userId)
-  }, [])
+    getAllItems();
+
+    localStorage.setItem("userId", userId);
+  }, []);
 
   return (
     <div className="flex  flex-col gap-y-6 justify-center shadow-lg items-center w-screen h-screen">
@@ -90,7 +97,7 @@ export default function Main() {
                 <input
                   type="text"
                   name="searchQuery"
-                   onChange={(e) => handleSearch(e.target.value)}
+                  onChange={(e) => handleSearch(e.target.value)}
                   placeholder="search here"
                   className=" text-center  backdrop-blur-md w-full h-[60px] bg-primary rounded-md  flex px-3 justify-center items-center outline-none"
                 />
@@ -113,12 +120,13 @@ export default function Main() {
           )}
 
           {createNewTaskFlag && <URLInput />}
-          { 
-            loading ?
-              <Spinner /> : (
-                  error ? (<h1 className=" text-red-700 ">Error while fetching data</h1>) : ( <TodoList /> )
-              )
-          }
+          {loading ? (
+            <Spinner />
+          ) : error ? (
+            <h1 className=" text-red-700 ">Error while fetching data</h1>
+          ) : (
+            <TodoList />
+          )}
           <SignOut />
           <ToggleSearch
             setToggleSearch={setToggleSearch}
@@ -135,5 +143,5 @@ export default function Main() {
         extension for this web app
       </a>
     </div>
-  )
+  );
 }
